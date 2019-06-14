efb4fdf0928632ef01a525311875fb9a917cb07f
Nico Alimin <nico@hackcapital.com>
2019-06-14 14:19:33 -0700

    Added script to ssh

--- src/index.sh
+++ src/index.sh
@@ -1,13 +1,26 @@
 #!/bin/bash
 
-# Ensure / get AWS credentials
-aws sts get-caller-identity
-
-# Check for tf.py
-cat src/model.py
+UUID=tensorflow-$(uuidgen)
 
 # Run CloudFormation
-aws cloudformation create-stack --stack-name tf-stack --template-body file://cf-template.json
+aws cloudformation create-stack --stack-name $UUID --template-body file://src/cf-template.json >/dev/null
+
+echo "Waiting for stack to be created, hang on!"
+
+aws cloudformation wait stack-create-complete --stack-name $UUID
+
+# Gets the resource ID
+RESOURCE_ID=$(aws cloudformation describe-stack-resource --stack-name $UUID --logical-resource-id HelloBucket | jq ".StackResourceDetail.PhysicalResourceId" | sed 's/^"\(.*\)"$/\1/')
+
+echo "Waiting for the instance to finish initializing"
+
+aws ec2 wait instance-status-ok --instance-ids $RESOURCE_ID
+
+# Gets the Public IP
+PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $RESOURCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+
+# Appened these before any commands you want to run in the instance
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP
 
 # Run SSH commands to:
 #
