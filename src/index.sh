d2d4995f10ca238f9857af5d6866fe3cc957bcab
Nico Alimin <nico@hackcapital.com>
2019-06-14 14:31:52 -0700

    Added the python ssh commands

--- src/index.sh
+++ src/index.sh
@@ -20,7 +20,15 @@ aws ec2 wait instance-status-ok --instance-ids $RESOURCE_ID
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $RESOURCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 
 # Appened these before any commands you want to run in the instance
-ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP "sudo apt-get install software-properties-common"
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP "sudo add-apt-repository ppa:deadsnakes/ppa"
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP "sudo apt-get update"
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP "sudo apt-get install -y python3.6"
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP "sudo apt install python-pip"
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP "sudo pip install --upgrade pip"
+ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP "pip install --ignore-installed tensorflow==2.0.0-beta0"
+
+
 
 # Run SSH commands to:
 #
@@ -29,8 +37,6 @@ ssh -i ./src/tensorflowthon.pem ec2-user@$PUBLIC_IP
 # - Report back with results.
 
 # Run cloud formation delete stack
-aws cloudformation delete-stack --stack-name foo
-
 
 # ops aws-tensorflow report
 
