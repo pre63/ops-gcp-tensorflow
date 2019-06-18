7176aa89f4e99aec2959de9a557d9cd0827ab431
Simon <simon@hackcapital.com>
2019-06-18 16:44:39 -0700

    many small changes

--- src/scripts/create.sh
+++ src/scripts/create.sh
@@ -2,65 +2,48 @@
 
 UUID=$1
 
+echo
 echo "Starting the cloudformation stack, hang on..."
 
 # Run CloudFormation
 aws cloudformation create-stack --stack-name $UUID --template-body file://src/cf-template.json >/dev/null
 
+echo
 echo "Starting to initialize instances..."
 
 # Wait for the stack to complete it's creation
-aws cloudformation wait stack-create-complete --stack-name $UUID
+aws cloudformation wait stack-create-complete --stack-name $UUID >/dev/null
 
 # Gets the resource ID
-RESOURCE_ID=$(aws cloudformation describe-stack-resource --stack-name $UUID --logical-resource-id TensorflowInstance | jq ".StackResourceDetail.PhysicalResourceId" | sed 's/^"\(.*\)"$/\1/')
+RES_TMP=$(aws cloudformation describe-stack-resource --output json --stack-name $UUID --logical-resource-id TensorflowInstance)
+RES_TMP2=$(jq -n "$RES_TMP" | jq .StackResourceDetail.PhysicalResourceId)
+RESOURCE_ID=$(echo $RES_TMP2 | sed 's/^"\(.*\)"$/\1/')
 
-echo "Waiting for the instance to finish initializing..."
-aws ec2 wait instance-status-ok --instance-ids $RESOURCE_ID
+echo
+echo "Waiting for the instance to start..."
+aws ec2 wait instance-status-ok --instance-ids $RESOURCE_ID >/dev/null
 
 # Gets the Public IP
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids $RESOURCE_ID --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
 
-echo "The following output adds the instance to the known hosts"
-ssh-keyscan -H $PUBLIC_IP >> ~/.ssh/known_hosts
+echo
+echo "Sucessfully created cloudfront stack."
 
 # Allows the pem key to be read only by the user
 chmod 400 /root/creds/tensorflow.pem
 
-# echo "Installing the python environment on the instance..."
+echo
+echo "Running tensorflow code..."
 
-# # # Appened these before any commands you want to run in the instance
-# ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "sudo yum update -y" >/dev/null
-# ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "sudo yum install python36 -y" >/dev/null
-# ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "curl -O https://bootstrap.pypa.io/get-pip.py" >/dev/null
-
-
-# ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "python get-pip.py --user" >/dev/null
-# RC=$?
-# if [ "$RC" != "0" ]; then
-#   ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "sudo python get-pip.py --user" >/dev/null
-# fi
-
-# ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "pip install --upgrade pip" >/dev/null
-# RC=$?
-# if [ "$RC" != "0" ]; then
-#   ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "sudo pip install --upgrade pip" >/dev/null
-# fi
-
-# ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "pip install --ignore-installed tensorflow==2.0.0-beta1 --user" >/dev/null
-# RC=$?
-# if [ "$RC" != "0" ]; then
-#   ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "sudo pip install --ignore-installed tensorflow==2.0.0-beta1 --user" >/dev/null
-# fi
-
-echo "Running your python tensorflow code..."
-
-# Transfers the tensorflow.py file to the container
-scp -i /root/creds/tensorflow.pem ./src/tensorflow.py ec2-user@$PUBLIC_IP:/home/ec2-user
+# Transfers the tf.py file to the container
+scp -q -o StrictHostKeyCHecking=no -i /root/creds/tensorflow.pem ./src/tf.py ubuntu@$PUBLIC_IP:/home/ubuntu
 
 # Runs the newly-transferred file using python3 and tensorflow2.0
 # TODO: Confirm whether this is using tensorflow2.0
-ssh -i /root/creds/tensorflow.pem ec2-user@$PUBLIC_IP "python tensorflow.py"
+ssh -q -o StrictHostKeyCHecking=no -i /root/creds/tensorflow.pem ubuntu@$PUBLIC_IP "python tf.py"
 
-echo "Sucessfully created the instance with name $UUID"
-echo "You can SSH into the instance by executing the following `ssh -i ~/creds/tensorflow.pem ec2-user@$PUBLIC_IP`"
+echo
+echo "Stack name $UUID"
+echo "To SSH into the instance execute the following"
+echo "ssh -i ~/creds/tensorflow.pem ubuntu@$PUBLIC_IP"
+
--- src/scripts/destroy.sh
+++ src/scripts/destroy.sh
@@ -2,9 +2,11 @@
 
 UUID=$1
 
+echo
 echo "Destroying the instance after use"
 
 # Run cloud formation delete stack
 aws cloudformation delete-stack --stack-name $UUID
 
+echo
 echo "Finished destroying instance!"
